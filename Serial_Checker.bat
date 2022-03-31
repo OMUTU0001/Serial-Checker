53e91c86e280c2b1e688a32b256002f07048a297
OMUTU <97812637+OMUTU0001@users.noreply.github.com>
2022-03-31 17:25:16 +0900

    Add files via upload

---
+++ Serial_Checker.bat
@@ -0,0 +1,43 @@
+:a
+@echo off
+title OMUTU#4545
+mode con: cols=70 lines=60
+color 08
+echo                                                 MU-NI- Serial Checker
+echo [94mBaseboard
+echo [93m========================[92m
+wmic baseboard get serialnumber
+echo [94mBios
+echo [93m========================[92m
+wmic bios get serialnumber
+echo [94mCPU
+echo [93m========================[92m
+wmic cpu get serialnumber
+echo [94mDiskdrive (#1)
+echo [93m========================[92m
+wmic diskdrive get serialnumber
+echo [94mDiskdrive (#2)
+echo [93m========================[92m
+wmic path win32_physicalmedia get SerialNumber
+echo [94mDiskdrive (#3)
+echo [93m========================[92m
+wmic path win32_diskdrive get SerialNumber
+echo [94mBaseboard
+echo [93m========================[92m
+wmic csproduct get uuid
+echo [94mRAM
+echo [93m========================[92m
+wmic memorychip get serialnumber
+echo [94mCPU
+echo [93m========================[92m
+wmic cpu get processorid
+echo [94mBaseBoard
+echo [93m========================[92m
+wmic baseboard get manufacturer
+echo [94mMac 
+echo [93m========================[92m
+wmic path Win32_NetworkAdapter where "PNPDeviceID like '%%PCI%%' AND NetConnectionStatus=2 AND AdapterTypeID='0'" get MacAddress
+echo.
+pause
+
+goto a
